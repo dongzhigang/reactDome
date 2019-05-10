5f4fea29c2bc770d9acd842f26863698d796d55a
dongzhigang <13632482567@163.com>
2019-05-10 15:18:47 +0800

    路径

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,3 +1,2 @@
-// const path = "http://139.159.224.247/food_sys/api/"; //服务器
-const path = "http://192.168.1.8/restaurantAdmin/api/";    //本地
+const path = "http://192.168.1.74/reactDomeAdmin/api/";    //本地
 export default path;
